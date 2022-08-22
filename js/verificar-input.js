40c4a676526a31ca072defd66d7db69a3d070f9e
Ale <ale.salas271102@gmail.com>
2022-08-22 04:56:19 -0300

    prueba 12

--- js/verificar-input.js
+++ js/verificar-input.js
@@ -1,12 +1,16 @@
 'use strict'
-const input =document.getElementById('input-nueva-palabra');
-input.addEventListener('input',()=>{
+const input = document.getElementById('input-nueva-palabra');
+input.addEventListener('input', () => {
+    if (input.value.length > 10) {
+        let palabra = input.value.replace(/.$/, '')
+        input.value = palabra;
+    }
     let letra = input.value.substr(-1);
     if (restricciones(letra) || letra === ' ') {
-        input.value = input.value.replace(/.$/,'');
+        input.value = input.value.replace(/.$/, '');
     }
     verificarInput();
-}) 
+})
 const verificarInput = function () {
     let input = document.getElementById('input-nueva-palabra');
     input.addEventListener("keydown", event => {
